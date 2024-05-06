f48ce81810993ba594d5b04f2536ce2b0f4f3b27
Zaynab Azzouzi <zazzouzi@Zaynabs-MacBook-Pro.local>
2024-05-06 19:36:11 +0200

    other screens

---
+++ screens/SaturdayEntry.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, SafeAreaView } from "react-native";
+
+export default function SaturdayEntry() {
+    return (
+        <SafeAreaView style={styles.container}>
+          <View style = {styles.topContainer}>
+          <Text style = {styles.text}>
+            What is a message you want to give to your future self?
+          </Text>
+          </View>
+      
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#191259',
+    marginBottom: 16,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "FFFFFF",
+    justifyContent: 'space-between',
+  },
+  topContainer: {
+    alignitems: 'center',
+    padding: 16,
+  },
+  
+});
---
+++ screens/TuesdayEntry.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, SafeAreaView } from "react-native";
+
+export default function TuesdayEntry() {
+    return (
+        <SafeAreaView style={styles.container}>
+          <View style = {styles.topContainer}>
+          <Text style = {styles.text}>
+          Describe a book or movie that changed your perspective on life
+          </Text>
+          </View>
+      
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#191259',
+    marginBottom: 16,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "FFFFFF",
+    justifyContent: 'space-between',
+  },
+  topContainer: {
+    alignitems: 'center',
+    padding: 16,
+  },
+  
+});
